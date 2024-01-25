da0ce165c30405f62b1509c08d736f261135236b
neg2004ah <neginahmadi533@gmail.com>
2024-01-25 16:57:14 +0330

    complete api

--- todo/api/V1/serializer.py
+++ todo/api/V1/serializer.py
@@ -5,17 +5,9 @@ from ...models import Task
 class TaskSerializer(serializers.ModelSerializer):
    
         
-    def to_representation(self, instance):
-        rep = super().to_representation(instance)
-        request = self.context.get('request')
-        kwargs = request.parser_context.get('kwargs')
-        if kwargs.get('pk') is not None:
-            rep.pop('content')
-        return rep
-    
     def create(self, validated_data):
-        validated_data['user'] = self.context.get('request').user
         
+        validated_data['user'] = self.context.get('request').user
         return super().create(validated_data)    
     
     class Meta:
