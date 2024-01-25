0d57923775953a64ae2248ab2f7caf55b20bcd49
hamid reza <126154017+hamidreza1983@users.noreply.github.com>
2024-01-26 02:11:33 +0330

    edit

--- todo/api/V1/serializer.py
+++ todo/api/V1/serializer.py
@@ -5,14 +5,10 @@ from ...models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
-    
-    def create(self, validated_data):
-        alidated_data['user'] = self.context.get('request').user
-        return super().create(validated_data) 
-
-class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ["user" ,"title"]
+    
+    
   
   
