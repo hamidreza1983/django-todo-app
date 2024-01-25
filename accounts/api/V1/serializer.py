dabcd62419ab12e88d034b7c7271a741e28da3d6
parhamgnox <137355548+parhamgnox@users.noreply.github.com>
2024-01-25 19:55:04 +0330

    final

--- accounts/api/V1/serializer.py
+++ accounts/api/V1/serializer.py
@@ -2,6 +2,7 @@ from ...models import CustomeUser
 from rest_framework import serializers
 
 from django.contrib.auth.password_validation import validate_password
+
 from django.core import exceptions
 
 
