4e5b2725eb0e60791694e1a404d4ebb359a84d76
parhamgnox <137355548+parhamgnox@users.noreply.github.com>
2024-01-25 18:42:40 +0330

    final

--- accounts/models.py
+++ accounts/models.py
@@ -1,14 +1,7 @@
 from django.db import models
-
-from django.db import models
 from django.contrib.auth.models import AbstractUser, AbstractBaseUser,BaseUserManager,PermissionsMixin
 
 
-
-# class CustomeUser(AbstractUser):
-#     id_code = models.CharField(max_length=10,null=True, blank=True)
-#     mobile = models.CharField(max_length=20,null=True, blank=True)
-#     image = models.ImageField(upload_to='users', default='user.jpg')
 
 class CustomeBaseUserManager(BaseUserManager):
     def create_user(self, email, password, **extra_fields):
@@ -48,7 +41,3 @@ class CustomeUser(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
-    
-
-    def user_tasks(self):
-        return self.tasks.all()
