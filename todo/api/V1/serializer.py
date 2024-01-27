34a75b5e1491fc4e4e859f4c34d43bf150d66b97
MaryamKalantarii <maryamnkalantaria72@gmail.com>
2024-01-27 20:11:37 +0330

    todo serializer

--- todo/api/V1/serializer.py
+++ todo/api/V1/serializer.py
@@ -7,6 +7,10 @@ from accounts.models import CustomeUser
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField()
+    def create(self, validated_data):
+        validated_data['user'] = self.context.get('request').user
+        return super().create(validated_data)    
 
     class Meta:
         model = Task
