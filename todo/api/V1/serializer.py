b4120a8e9a5a69afd66c6b915afa2e676c02a6d8
MaryamKalantarii <maryamnkalantaria72@gmail.com>
2024-01-27 23:46:24 +0330

    edit

--- todo/api/V1/serializer.py
+++ todo/api/V1/serializer.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
 from ...models import Task
-from accounts.models import CustomeUser
+
 
 
 
 
 
 class TaskSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField()
+    
     def create(self, validated_data):
         validated_data['user'] = self.context.get('request').user
         return super().create(validated_data)    
@@ -15,21 +15,8 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields =[
-            "id","user", "title" , "complete", 
+            "title" , "complete", 
         ]
     
-    def to_representation(self, instance):
-        rep = super().to_representation(instance)
-        rep['user'] = UserSerializer(instance.user).data
-        return rep
-
-
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomeUser
-        fields = [
-            "email" ,"username" 
-        ]
+   
 
