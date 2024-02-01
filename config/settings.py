ee61e95eb861f29168a83633dfbdcc6d5eb72595
MaryamKalantarii <maryamnkalantaria72@gmail.com>
2024-02-01 21:01:55 +0330

    .

--- config/settings.py
+++ config/settings.py
@@ -198,4 +198,4 @@ SIMPLE_JWT = {
 if DEBUG:
     import socket  # only if you haven't already imported this
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "172.25.0.2"]
