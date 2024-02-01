ff76072a91cdc72c8d96a99eb429969f730e6f46
Ayda <aydaghasemi66@gmail.com>
2024-02-01 21:26:36 +0330

    django and havaye barfi

--- config/settings.py
+++ config/settings.py
@@ -149,8 +149,8 @@ REST_FRAMEWORK = {
     ]
 }
 
-INTERNAL_IPS = [
-    
-    "127.0.0.1",
-    
-]
+
+if DEBUG:
+    import socket  # only if you haven't already imported this
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "172.26.0.2"]   
