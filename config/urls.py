95cfedbe8c089789ad7c91d95ae488cd90cbff87
neg2004ah <neginahmadi533@gmail.com>
2024-02-01 21:00:13 +0330

    Django debugger just in develpment

--- config/urls.py
+++ config/urls.py
@@ -44,10 +44,18 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/',include('django.contrib.auth.urls')),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-   path("__debug__/", include("debug_toolbar.urls")),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
+
+
+from .setting.development import DEBUG
+
+if DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
 
 
 urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
