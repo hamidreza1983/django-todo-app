633a24e47886925a62a693214cfd23ae20f3e172
mohadese doosti <137603928+mhidusti@users.noreply.github.com>
2024-02-26 00:48:47 +0330

    clean code

--- accounts/api/views/editprofile.py
+++
@@ -1,9 +0,0 @@
-from ..models import  Profile
-from django.views.generic import UpdateView
-
-
-class EditProfileView(UpdateView):
-    template_name = 'registration/edit_profile.html'
-    model = Profile
-    fields = [ 'user','first_name', 'last_name', 'image', 'phone', 'address']
-    success_url = '/'
