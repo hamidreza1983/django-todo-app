c2fb64524e494aaf744247291457e55eb69403ba
Sobhan Moradi <sobhan.mrds@gmail.com>
2024-03-01 22:45:12 +0330

    Create Tests

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.urls import resolve, reverse
+from todo.views import(
+    HomeView,
+    DeleteTask,
+    CompleteTask,
+    UpdateTask,
+)
 
-# Create your tests here.
+class TestTodoURL(TestCase):
+    '''Tests for the todo/urls.py file'''
+    def test_HomeUrl(self):
+        url = reverse('todo:home')
+        self.assertEqual(resolve(url).func.view_class, HomeView)
+    
+    def test_DeleteTaskUrl(self):
+        url = reverse('todo:delete_task', kwargs={'pk': 1})
+        self.assertEqual(resolve(url).func.view_class, DeleteTask)
+
+    def test_CompleteTaskUrl(self):
+        url = reverse('todo:complete_task', kwargs={'pk': 1})
+        self.assertEqual(resolve(url).func.view_class, CompleteTask)
+
+    def test_UpdateTaskUrl(self):
+        url = reverse('todo:update_task', kwargs={'pk': 1})
+        self.assertEqual(resolve(url).func.view_class, UpdateTask)
+
+    
